b606e6f5a9acaa9a19065849545d318a5e4f8d15
lucasgo1099@gmail.com <you@example.com>
2022-04-05 00:46:57 -0300

    add configure models database

--- lib/Database/database1.dart
+++ lib/Database/database1.dart
@@ -1,26 +1,41 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+import '../Models/Register_user_model.dart';
+
 class Connection {
-  Database? _database;
+  Database? database;
 
 //metodo para retornar a conexão
   Future<Database?> getDb() async {
-    if (_database == null) {
+    if (database == null) {
       // criando o caminho do banco de dados
       var path = join(await getDatabasesPath(), 'db_project');
       //abrindo banco de dados
-      _database = await openDatabase(
+      database = await openDatabase(
         path,
         version: 1,
         //o que é pra ser criado
         onCreate: (bd, v) {
           //criação da tabela
-          bd.execute(
-              'CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)');
+          bd.execute('''
+              CREATE TABLE user(
+                id INTEGER PRIMARY KEY, name TEXT, lastname TEXT, email TEXT, passaword TEX, phone NUM, cep NUM,image TEXT
+              )
+          ''');
         },
       );
     }
-    return _database;
+    return database;
+  }
+
+  Future close() async {
+    database?.close();
+  }
+
+  Future<void> adduser({required Usermodel user}) async {
+    //return await database?.insert('user', Usermodel.fromJson(uuario));
+    await database?.rawInsert(
+        'INSERT INTO user(name, lastname, email, password, phone, cep, image) VALUES ("{$user.name}","{$user.lastname}", "{$user.email}", "{$user.password}, "{$user.phone}, "{$user.cep}. "{$user.image}",",", ", )');
   }
 }
